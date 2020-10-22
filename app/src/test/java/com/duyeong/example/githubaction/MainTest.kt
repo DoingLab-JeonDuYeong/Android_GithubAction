09d6107c8dcaecdc5de61014ace68527105e4582
전두영 <duyeong@doinglab.com>
2020-10-22 14:58:28 +0900

    Add Test

---
+++ app/src/test/java/com/duyeong/example/githubaction/MainTest.kt
@@ -0,0 +1,35 @@
+package com.duyeong.example.githubaction
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * @author duyeong@doinglab.com
+ */
+
+class MainTest {
+    @Test
+    fun test1() {
+        assertEquals("Test1", "Test1")
+    }
+
+    @Test
+    fun test2() {
+        assertEquals("Test2", "Test2")
+    }
+
+    @Test
+    fun test3() {
+        assertEquals("Test2", "Test3")
+    }
+
+    @Test
+    fun test4() {
+        assertEquals("Test123", "Test4")
+    }
+
+    @Test
+    fun test5() {
+        assertEquals("Test5", "Test5")
+    }
+}
